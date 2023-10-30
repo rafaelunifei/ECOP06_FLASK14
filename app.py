720b5f67c5d859671debd8a3cbbd4c2deb98750c
rafaelunifei <146854230+rafaelunifei@users.noreply.github.com>
2023-10-30 11:56:05 +0000

    primeiro

---
+++ app.py
@@ -0,0 +1,13 @@
+from flask import Flask, render_template, request
+app = Flask(__name__)
+
+# rota/home
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+  nome = None
+  if request.method == 'POST' and 'nome' in request.form:
+    nome = request.form['nome']
+  return render_template('index.html', nome=nome)
+
+app.run(debug=True)
